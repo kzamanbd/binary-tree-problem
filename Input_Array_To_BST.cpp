4605abc03633d0f120fb6af34a3ebd39740f67d2
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-01-25 14:13:09 +0600

    update array to bst create function

--- Input_Array_To_BST.cpp
+++ Input_Array_To_BST.cpp
@@ -5,69 +5,97 @@ class TreeNode
 {
 public:
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
 
-    TreeNode(int val)
-    {
+    TreeNode(int val) {
         this->val = val;
         this->left = NULL;
         this->right = NULL;
     }
 };
 
-TreeNode *convertBST(int arr[], int l, int r)
-{
-    if (l > r)
-    {
+TreeNode* convertBST(vector<int> arr, int l, int r) {
+    if (l > r) {
         return NULL;
     }
     int mid = (l + r) / 2;
-    TreeNode *root = new TreeNode(arr[mid]);
-    TreeNode *leftNode = convertBST(arr, l, mid - 1);
-    TreeNode *rightNode = convertBST(arr, mid + 1, r);
+    TreeNode* root = new TreeNode(arr[mid]);
+    TreeNode* leftNode = convertBST(arr, l, mid - 1);
+    TreeNode* rightNode = convertBST(arr, mid + 1, r);
     root->left = leftNode;
     root->right = rightNode;
     return root;
 }
 
-void levelOrderPrint(TreeNode *root)
-{
-    if (root == NULL)
-    {
+void levelOrderPrint(TreeNode* root) {
+    if (root == NULL) {
         return;
     }
-    queue<TreeNode *> q;
+    queue<TreeNode*> q;
 
     q.push(root);
 
-    while (!q.empty())
-    {
-        auto *f = q.front();
+    while (!q.empty()) {
+        auto* f = q.front();
         q.pop();
         cout << f->val << " ";
-        if (f->left)
-        {
+        if (f->left) {
             q.push(f->left);
         }
-        if (f->right)
-        {
+        if (f->right) {
             q.push(f->right);
         }
     }
 }
 
-int main()
-{
+void preorder(TreeNode* root) {
+    if (!root) {
+        return;
+    }
+
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(TreeNode* root) {
+    if (!root) {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << " ";
+}
+
+void inOrder(TreeNode* root) {
+    if (!root) {
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->val << " ";
+    inOrder(root->right);
+}
+
+int main() {
     int n;
     cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    TreeNode *root = convertBST(arr, 0, n - 1);
+    sort(arr.begin(), arr.end());
+    TreeNode* root = convertBST(arr, 0, n - 1);
+    cout << "level Order: ";
     levelOrderPrint(root);
+    cout << endl << "preorder: ";
+    preorder(root);
+    cout << endl << "postorder: ";
+    postorder(root);
+    cout << endl << "inOrder: ";
+    inOrder(root);
 
     return 0;
 }
